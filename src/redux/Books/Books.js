406a770d24fdeec97038972fe752fd6b6f4939fc
Andy Menutti <andymenutti@gmail.com>
2022-09-01 15:48:21 -0300

    add redux/ with Books/ Books.js & Category/Category.js + install redux for Provider & create configureStore.js

---
+++ src/redux/Books/Books.js
@@ -0,0 +1,56 @@
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+const initialState = [
+  {
+    title: 'The Hunger Games',
+    author: 'Author1',
+    id: 1,
+  },
+
+  {
+    title: 'Dune',
+    author: 'Author2',
+    id: 2,
+  },
+];
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          id: 1,
+          title: action.title,
+          author: 'action.author',
+        },
+      ];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+
+    default:
+      return state;
+  }
+};
+
+export const addBook = (titleBook, authorBook, idBook) => {
+  const Book = {
+    type: ADD_BOOK,
+    title: titleBook,
+    author: authorBook,
+    id: idBook,
+  };
+  return Book;
+};
+
+export const removeBook = (id) => {
+  const Book = {
+    type: REMOVE_BOOK,
+    id,
+  };
+  return Book;
+};
+
+export default booksReducer;
